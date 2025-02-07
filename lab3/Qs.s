993df749df80e0f5a9b2a9e28550cf64157ee1bb
Plugs <plugs2004@gmail.com>
2025-02-07 17:41:51 +0700

    ERROR - Result, Registers and the end almost correct

--- lab3/Qs.s
+++ lab3/Qs.s
@@ -1,63 +1,66 @@
-Data                DCD     10, 12, 8, 1, 5, 7, 11, 6, 8
-                    MOVS    R12, #0x08
-MAIN                LDR     R0, =Data
-                    MOVS    R10, #0x00
-                    BL      QUICKSORT
 
+Data                DCD     10, 12, 8, 1, 5, 7, 11, 6, 8 ; Example data
 
-QUICKSORT           CMP     R10, R12
+MAIN                LDR     R0, =Data ; Load address of data into R0
+                    MOVS    R1, #0x00 ; Initialize low index to 0
+                    MOVS    R2, #0x08 ; Number of elements - 1
+
+                    BL      QUICKSORT ; Call QuickSort
+                    B       EXIT
+
+QUICKSORT           
+                    CMP     R1, R2 ; If low >= high, return
                     BGT     NOTHING
-                    STMFD   R8!, {R0, R10, R12, LR}
-                    BL      PARTITION
-                    MOV     R9, R0 ;low index (first)
-                    MOV     R5, R12 ;high index (last)
+                    STMFD   R12!, {R3, LR} ; Save registers
 
+                    BL      PARTITION ; Call partition
 
-                    SUB     R12,R9,#1 ;HIGH = PI-1
-                    BL      QUICKSORT
+                    SUB     R2, R3, #1 ; HIGH = PI - 1
+                    BL      QUICKSORT ; Sort left partition
 
-                    LDMFD   R8!, {R0, R10, R12, LR} ;RESTORE CURRENT DATA
-                    ADD     R10, R9, #1 ; LOW = PI + 1
-                    BL      QUICKSORT
+                    LDMFD   R12!, {R3, LR} ; Restore registers
+                    ADD     R1, R3, #1 ; LOW = PI + 1
+                    BL      QUICKSORT ; Sort right partition
 
-                    LDMFD   R8!, {R0, R10, R12, LR} ;RESTORE LINK REGISTER
-                    MOV     PC, LR
+                    LDMFD   R12!, {R3, LR} ; Restore before returning
+                    MOV     PC, LR ; Return
 
+PARTITION           
+                    STMFD   R12!, {R6, R7, R8, LR} ; Save registers
+                    LDR     R3, [R0, R2, LSL #2] ; PIVOT = ARR[HIGH]
+                    SUB     R8, R2, #1 ;
 
-PARTITION           STMFD   R8!, {R4, R5, R6, LR}
-                    MOV     R4, R0 ;base address
-                    MOV     R5, R12 ;high index
-                    LDR     R9, [R4, R5, LSL #2] ;PIVOT =ARR[HIGH]
+                    MOV     R4, R1 ; j = LOW
+                    MOV     R5, R1 ; i = LOW
 
-                    MOV     R2, R10 ;j = LOW
-                    MOV     R3, R10 ;i = LOW
+PARTITION_LOOP      
+                    CMP     R4, R8 ; while j <= HIGH - 1
+                    BGT     SWAP ; End loop if j > high
 
-PARTITION_LOOP      CMP     R2,R5 ; j > HIGH
-                    BGT     SWAP
+                    LDR     R6, [R0, R4, LSL #2] ; ARR[J]
+                    CMP     R6, R3 ; IF (ARR[J] <= PIVOT)
+                    BLE     LESS_OR_EQUAL_PIVOT ; Swap(ARR[j], ARR[i])
 
-                    LDR     R7, [R4, R2, LSL #2] ;ARR[J]
-                    CMP     R7,R9 ;IF (ARR[J] > PIVOT)
-                    BLE     LESS_OR_EQUAL_PIVOT ; SWAP(ARR[R2], ARR[R3])
+                    ADD     R4, R4, #1 ; J++
+                    B       PARTITION_LOOP ; Repeat loop
 
-                    ADD     R2,R2,#1 ;J++
-                    B       PARTITION_LOOP
+LESS_OR_EQUAL_PIVOT 
+                    LDR     R7, [R0, R5, LSL #2] ; temp = ARR[i]
+                    STR     R6, [R0, R5, LSL #2] ; ARR[i] = ARR[j]
+                    STR     R7, [R0, R4, LSL #2] ; ARR[j] = temp
+                    ADD     R5, R5, #1 ; i++
+                    ADD     R4, R4, #1 ; j++
+                    B       PARTITION_LOOP ; Repeat loop
 
-LESS_OR_EQUAL_PIVOT LDR     R6, [R4, R3, LSL #2] ; temp = ARR[i]
-                    STR     R7, [R4, R3, LSL #2] ; ARR[i] = ARR[j]
-                    STR     R6, [R4, R2, LSL #2] ; ARR[j] = temp
-                    ADD     R3, R3, #1 ; i++
-                    ADD     R2, R2, #1 ; j++
-                    B       PARTITION_LOOP
+SWAP                
+                    LDR     R6, [R0, R5, LSL #2] ; R6 = ARR[i]
+                    STR     R3, [R0, R5, LSL #2] ; R3 -> Arr[i]
+                    STR     R6, [R0, R2, LSL #2] ; R6 -> Arr[high]
+                    MOV     R3, R5 ; Return pivot index
+                    LDMFD   R12!, {R6, R7, R8, LR} ; Restore registers
+                    MOV     PC, LR ; Return
 
-SWAP                LDR     R7, [R4, R3, LSL #2]
-                    STR     R9, [R4, R3, LSL #2]
-                    STR     R7, [R4, R5, LSL #2]
-                    MOV     R0, R3
-                    LDMFD   R8!, {R4, R5, R6, LR}
-                    MOV     PC, LR
+NOTHING             
+                    MOV     PC, LR ; Return if nothing to sort
 
-
-
-NOTHING             MOV     PC, LR
-
-
+EXIT                
